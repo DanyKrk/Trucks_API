ff5647ebbda4c5639539fa690c6d2597842a4a48
DanyKrk <daniel.k.8576@gmail.com>
2023-02-07 23:51:50 +0000

    Add initial migration

---
+++ Migrations/20230207234607_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrucksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tare = table.Column<int>(type: "int", nullable: false),
+                    LoadCapacity = table.Column<int>(type: "int", nullable: false),
+                    MaximumBatteryCharge = table.Column<int>(type: "int", nullable: false),
+                    AutonomyWhenFullyCharged = table.Column<int>(type: "int", nullable: false),
+                    FastChargingTime = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trucks");
+        }
+    }
+}
